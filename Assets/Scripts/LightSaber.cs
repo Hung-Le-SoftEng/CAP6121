bb3f1476beb5cc9018baf83aaae50786a27cb65b
Hung-Le-UCF <hungle.zero@yahoo.com>
2016-02-03 18:31:53 -0500

    First Upload

---
+++ Assets/Scripts/LightSaber.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSaber : MonoBehaviour {
+
+    LineRenderer lineRenderer;
+    public Transform startPos;
+    public Transform endPos;
+
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        lineRenderer.SetPosition(0, startPos.position);
+        lineRenderer.SetPosition(1, endPos.position);
+
+	}
+}
---
+++ Assets/Scripts/MonsterController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterController : MonoBehaviour {
+
+
+    private NavMeshAgent navAgent;
+    public Transform enemy;
+    Animator animator;
+
+    public float speed = 2f;
+    public float runSpeed = 4.5f;
+    float originalSpeed = 2f;
+    public float rotationSpeed = 120f;
+    public float dampTime = 3f;
+
+    public AudioSource myaudiosource;
+    public Transform target;
+    public Transform chest;
+    public Transform shield;
+    public Transform weapon;
+    public Transform leftHandPos;
+    public Transform rightHandPos;
+    public Transform chestPosShield;
+    public Transform chestPosWeapon;
+
+    public AudioClip equip1Sound;
+    public AudioClip equip2Sound;
+    public AudioClip holster1Sound;
+    public AudioClip holster2Sound;
+
+    public bool grounded = true;
+
+    public GameObject bullet;
+    public float bulletSpeed = 10f;
+    public bool shoot = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+        navAgent = transform.GetComponent<NavMeshAgent>();
+
+        navAgent.speed = speed;
+        navAgent.angularSpeed = rotationSpeed;
+
+        animator = transform.GetComponent<Animator>();
+        originalSpeed = speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (target != null) {
+            navAgent.SetDestination(enemy.position);
+            animator.SetBool("grounded", grounded);
+            animator.SetFloat("speed", navAgent.velocity.magnitude, dampTime, 0.2f);
+        }
+
+        if (shoot) {
+            GameObject gameObj = (GameObject) Instantiate(bullet, transform.position + transform.forward, transform.rotation);
+
+            Rigidbody rb = gameObj.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * bulletSpeed;
+
+            shoot = false;
+        }
+
+	}
+}
